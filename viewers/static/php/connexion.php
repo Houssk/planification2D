87d3c4fe51459c3f818e1232c2f061e0ff74e9b6
Nicolas Odin <nodin@oneortho-medical.com>
2016-07-04 18:01:28 +0200

    Reprise du design de la partie implant (images et taille)

--- viewers/static/php/connexion.php
+++ viewers/static/php/connexion.php
@@ -1,8 +1,8 @@
 <?php
 	// On se connecte à la BDD
-	$dbhost="localhost";
+	$dbhost="127.0.0.1:3306";
 	$dbuser="root";
-	$dbpass="";
+	$dbpass="On30rth0M3d!cal";
 
 	$dblink=mysql_connect($dbhost,$dbuser,$dbpass);
 	mysql_select_db("planif2d_serf",$dblink);
