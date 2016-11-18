c38735735f43c276540cdb6729f61694e7954099
Houssk <karrach.houssam@gmail.com>
2016-11-18 10:09:52 +0100

    modified index.html

--- viewers/static/php/connexion.php
+++ viewers/static/php/connexion.php
@@ -1,8 +1,8 @@
 <?php
 	// On se connecte à la BDD
-	$dbhost="localhost";
-	$dbuser="root";
-	$dbpass="";
+    $dbhost="127.0.0.1:3306";
+    $dbuser="root";
+    $dbpass="";
 
 	$dblink=mysql_connect($dbhost,$dbuser,$dbpass);
 	mysql_select_db("planif2d_serf",$dblink);
