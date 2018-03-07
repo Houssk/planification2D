413f21cd83525a2151b509f3197e186326e51344
Houssam karrach <houssam.karrach@telecom-st-etienne.fr>
2018-03-07 14:14:33 +0100

    Add connection.php to gitignore

--- viewers/static/php/connexion.php
+++ viewers/static/php/connexion.php
@@ -1,8 +1,8 @@
 <?php
 	// On se connecte à la BDD
     $dbhost="127.0.0.1:3306";
-    $dbuser="root";
-    $dbpass="";
+	$dbuser="root";
+	$dbpass="On30rth0M3d!cal";
 
 	$dblink=mysql_connect($dbhost,$dbuser,$dbpass);
 	mysql_select_db("planif2d_serf",$dblink);
