dbbfc7323f2a977fe645e7eabdaa4bedc63e5cec
Anthony-C <anthony.casanave@gmail.com>
2020-02-19 13:07:34 +0100

    [Fix] getTranslation now get URL from iframe parent, but have to use the iframe src /planif2d_dedienne/viewers/static/.

--- viewers/static/js/textTranslationManager.js
+++ viewers/static/js/textTranslationManager.js
@@ -17,8 +17,9 @@
 
 const lang = getLanguage();
 function getLanguage(){
+    console.log(window.location.pathname + " + " + window.location.pathname.split('/')[1]);
     const translatedLanguages = ["fr", "en"], //Pour ajouter une langue, ajoutez simplement ce qui apparait dans l'URL de la page en se connectant avec la langue ciblée. (/en/ deviens donc "en")
-          urlLanguage =  (window.location.pathname.split('/')[0].toLowerCase());
+          urlLanguage =  ($("iframe[src*='/planif2d_dedienne/viewers/static/']").contentWindow.location.pathname.split('/')[1].toLowerCase());
     return (translatedLanguages.indexOf(urlLanguage) >= 0) ? urlLanguage : "fr"; //En cas de traduction non terminée ou d'erreur de changement dans l'URL, la page sera par défaut en Français.
 }
 const translatedTexts = {
