77c59a593ca9890781875d5d2e0e172ce3656197
Anthony-C <anthony.casanave@gmail.com>
2020-08-03 17:30:17 +0200

    [Fix] Removing the database infos from git.

--- viewers/static/php/connexion.php
+++ viewers/static/php/connexion.php
@@ -1,8 +1,8 @@
 <?php
 	// On se connecte à la BDD
     $dbhost="127.0.0.1";
-	$dbuser="serf_prod_gestion";
-	$dbpass="h4LgNLrG4LcgDypw";
+	$dbuser="root";
+	$dbpass="";
 	$dblink=mysql_connect($dbhost,$dbuser,$dbpass);
 	
 	mysql_select_db("planif2d_serf",$dblink);
