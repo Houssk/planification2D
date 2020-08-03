193fb6fbba8752a39ab34a823152d77fdd442251
HOUSSAM KARRACH <karrach.houssam@gmail.com>
2020-08-03 12:35:31 +0200

    Delete connexion.php

--- viewers/static/php/connexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// On se connecte à la BDD
-    $dbhost="127.0.0.1";
-	$dbuser="serf_prod_gestion";
-	$dbpass="h4LgNLrG4LcgDypw";
-	$dblink=mysql_connect($dbhost,$dbuser,$dbpass);
-	
-	mysql_select_db("planif2d_serf",$dblink);
-?>
